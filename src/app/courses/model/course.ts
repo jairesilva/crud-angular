0caf3fd21698cfd771f8754e83c31786521ef921
Jair Estanislau da Silva <jairesilva@gmail.com>
2023-06-04 20:00:19 -0300

    Aula 50

--- src/app/courses/model/course.ts
+++ src/app/courses/model/course.ts
@@ -1,5 +1,8 @@
+import { Lesson } from './lesson';
 export interface Course {
   _id: string;
   name: string;
   category: string;
+  // ? = operador Elvis = deixa opcional
+  lesson?: Lesson[];
 }
